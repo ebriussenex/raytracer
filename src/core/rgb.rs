42f4306e7a4560a9fb6093ebfe06b22bae37284e
ebriussenex <ebriussenex@gmail.com>
2025-01-28 03:42:22 +0300

    gamma correction

--- src/core/rgb.rs
+++ src/core/rgb.rs
@@ -8,10 +8,20 @@ pub struct Rgb {
     rgb: [f64; 3],
 }
 
+// gamma 2 transformation
+fn linear_to_gamma(linear: f64) -> f64 {
+    if linear > 0.0 {
+        f64::sqrt(linear)
+    } else {
+        0.0
+    }
+}
+
 impl std::fmt::Display for Rgb {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let u8r = self
             .rgb
+            .map(linear_to_gamma)
             .map(|x| (x * 255.999).clamp(0.0, 256.0).round() as u8);
         writeln!(f, "{} {} {}", u8r[0], u8r[1], u8r[2])
     }
